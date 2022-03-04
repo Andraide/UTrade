1b8da4dbe6f293737c89575cade4d9040f4d191f
Cristobal Andrade Steil <cristobalsteil@MacBook-Pro-de-Cristobal.local>
2022-03-04 14:53:58 -0300

    feat: statusbar

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,9 +1,41 @@
-import React, { useState } from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, Platform, StatusBar, Dimensions } from "react-native";
+import SearchBar from "react-native-dynamic-search-bar"
+import DeviceInfo from "react-native-device-info"
+import { not } from "react-native-reanimated";
+const heigthScreen = Dimensions.get('window').height
 
 const Home = () => {
 
-    return <View><Text>Home</Text></View>
+    const [ notch, setNotch ] = useState(false)
+    const [ notchHeight, setNotchHeigth ] = useState(0)
+
+    useEffect(() => {
+        
+        if(Platform.OS == 'ios' )
+        {
+            setNotch(true)
+            setNotchHeigth(heigthScreen/20)
+        }
+        else if(true)
+        {
+            setNotch(true)
+            setNotchHeigth(StatusBar.currentHeight)
+        }
+
+        console.log("Status bar", StatusBar.currentHeight, heigthScreen)
+    })
+
+    return (
+            <View style={{ flex: 1, marginTop: notch ? notchHeight : 0 }}>
+                <View style={{ flex: 0.1 }}>
+                    <SearchBar/>
+                </View>
+                <View style={{ flex: 0.2 }}></View>
+                <View style={{ flex: 0.7 }}></View>
+
+            </View>
+    )
 }
 
 export { Home }
